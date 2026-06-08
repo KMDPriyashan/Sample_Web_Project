423e3f698338fd7b54592fea098eca953240242f
K.M.D.Priyashan <kmdpriyashanofficials2001@gmail.com>
2026-06-08 22:07:52 +0530

    Q6 --> Question Answers ✏️

---
+++ demo03.java
@@ -0,0 +1,24 @@
+//Q6 --> Question Answers
+
+
+class box{
+    int length;
+    int width;
+    int heigth;
+}
+public class demo03 {
+    public static void main(String args[]){
+
+        box b1 = new box();
+
+        b1.length=12;
+        b1.width=5;
+        b1.heigth=3;
+
+        System.out.println("Length :"+b1.length);
+        System.out.println("width :"+b1.width);
+        System.out.println("height :"+b1.heigth
+
+        );
+    }
+}
